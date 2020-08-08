b0a397bef048a675f54f2e1e426879ecaaf3e4c7
samir purno <purno.samir@gmail.com>
2020-08-08 23:33:04 +0200

    online statistics calculator

--- main.py
+++ main.py
@@ -1,19 +1,69 @@
-from flask import Flask, request
-
+from flask import Flask, request, redirect, url_for
+import statistics
+# create app
 app = Flask(__name__)
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return 'Usage;\n<Operation>?A=<Value1>&B=<Value2>\n'
+    if request.method == 'GET':
+        # show html form
+        return '''
+            <form method="post">
+                X: <input type="text" name="X" /
+                <p><input type="submit" name="operator" value="mean" />
+                <input type="submit" name="operator" value="median" />
+                <input type="submit" name="operator" value="maxi" />
+                <input type="submit" name="operator" value="mini" />
+            </form>
+        '''
+    elif request.method == 'POST':
+# calculate result
+        a = request.form.get('operator')
+        if a == 'mean':
+            X = request.form.get('X')
+            return redirect(url_for('mean', X=X,))
+            
+        if a == 'median':
+            X = request.form.get('X')
+            return redirect(url_for('median', X=X,))
 
+        if a == 'maxi':
+            X = request.form.get('X')
+            return redirect(url_for('maxi', X=X,))
 
-@app.route('/add')
-def addition():
-    value1=request.args.get('A',default = 0, type = int)
-    value2=request.args.get('B',default = 0, type = int)
-    result=value1+value2
-    return '%d \n' % result
+        if a == 'mini':
+            X = request.form.get('X')
+            return redirect(url_for('mini', X=X,))
 
+@app.route('/mean')
+def mean():
+    dict = request.args.to_dict()
+    X = eval(dict['X'])
+    result = statistics.mean(X)
+    return 'result: %s' % result
 
-if __name__ == "__main__":
-    app.run()
+@app.route('/median')
+def median():
+    dict = request.args.to_dict()
+    X = eval(dict['X'])
+    result = statistics.median(X)
+    return 'result: %s' % result
+
+@app.route('/maxi')
+def maxi():
+    dict = request.args.to_dict()
+    X = eval(dict['X'])
+    result = max(X)
+    return 'result: %s' % result
+
+@app.route('/mini')
+def mini():
+    dict = request.args.to_dict()
+    X = eval(dict['X'])
+    result = min(X)
+    return 'result: %s' % result
+
+# run app
+if __name__ == '__main__':
+    app.run(debug=True)
