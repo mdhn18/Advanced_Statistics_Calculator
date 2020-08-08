5dc3478e14a11e33f27f061990ae4c7a9e6bb0df
samir purno <purno.samir@gmail.com>
2020-08-08 23:41:54 +0200

    Average value

--- main.py
+++ main.py
@@ -12,9 +12,7 @@ def index():
             <form method="post">
                 X: <input type="text" name="X" /
                 <p><input type="submit" name="operator" value="mean" />
-                <input type="submit" name="operator" value="median" />
-                <input type="submit" name="operator" value="maxi" />
-                <input type="submit" name="operator" value="mini" />
+
             </form>
         '''
     elif request.method == 'POST':
@@ -24,17 +22,6 @@ def index():
             X = request.form.get('X')
             return redirect(url_for('mean', X=X,))
             
-        if a == 'median':
-            X = request.form.get('X')
-            return redirect(url_for('median', X=X,))
-
-        if a == 'maxi':
-            X = request.form.get('X')
-            return redirect(url_for('maxi', X=X,))
-
-        if a == 'mini':
-            X = request.form.get('X')
-            return redirect(url_for('mini', X=X,))
 
 @app.route('/mean')
 def mean():
@@ -43,26 +30,6 @@ def mean():
     result = statistics.mean(X)
     return 'result: %s' % result
 
-@app.route('/median')
-def median():
-    dict = request.args.to_dict()
-    X = eval(dict['X'])
-    result = statistics.median(X)
-    return 'result: %s' % result
-
-@app.route('/maxi')
-def maxi():
-    dict = request.args.to_dict()
-    X = eval(dict['X'])
-    result = max(X)
-    return 'result: %s' % result
-
-@app.route('/mini')
-def mini():
-    dict = request.args.to_dict()
-    X = eval(dict['X'])
-    result = min(X)
-    return 'result: %s' % result
 
 # run app
 if __name__ == '__main__':
