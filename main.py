73a241fc36ff7ded8650d742e58823823d82412f
samir purno <purno.samir@gmail.com>
2020-12-12 23:15:42 +0100

    update max

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from flask import Flask, request, redirect, url_for
+from flask import Flask, request
+from fractions import Fraction
 import statistics
 # create app
 app = Flask(__name__)
@@ -6,31 +7,36 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    if request.method == 'GET':
-        # show html form
-        return '''
-            <form method="post">
-                X: <input type="text" name="X" /
-                <p><input type="submit" name="operator" value="maximum" />
-
-            </form>
-        '''
-    elif request.method == 'POST':
-# calculate result
-        a = request.form.get('operator')
-        if a == 'maximum':
-            X = request.form.get('X')
-            return redirect(url_for('maximum', X=X,))
-
-
-@app.route('/maximum')
+    return 'Usage;\n<Operation>?X=<Value1, value2, value2, .... , valueN>\n'
+        
+@app.route('/max')
 def maximum():
-    dict = request.args.to_dict()
-    X = eval(dict['X'])
-    result = max(X)
-    return 'result: %s' % result
-
+    if request.method == 'POST':
+        inputs = request.values.get('X', default=0, type=str)
+        values = inputs.split(',')
+        
+        try:
+            values = [Fraction(x) for x in values]
+        except ZeroDivisionError as e:
+            print(ZeroDivisionError)
+        except ValueError as e:
+            print(ValueError)
+        except:
+            print(error)
+            
+        
+    else:
+        inputs = request.args.get('X', default=0, type=str)
+        values = inputs.split(',')
+        try:
+            values = [Fraction(x) for x in values]
+        except ZeroDivisionError as e:
+            print(ZeroDivisionError)
+        except ValueError as e:
+            print(ValueError)
+            
+    return str(float(max(values)))+"\n"
 
 # run app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
---
+++ tests.py
@@ -0,0 +1,35 @@
+
+import unittest
+import main
+
+
+
+class TestOnlineCalculator(unittest.TestCase):
+    """Testing features of online calculator"""
+
+    def setUp(self):
+        """Sets up the app for testing"""
+
+        main.app.testing = True
+        self.app = main.app.test_client() 
+
+    def test_empty_page(self):
+        """ Tests the page with no route"""
+        response_data = self.app.get('/')
+        self.assertEqual(b'Usage;\n<Operation>?X=<Value1, value2, value2, .... , valueN>\n', response_data.data)
+
+        # Testing POST Method
+        response_data = self.app.post('/')
+        self.assertEqual(b'Usage;\n<Operation>?X=<Value1, value2, value2, .... , valueN>\n', response_data.data)
+        
+    def test_max(self):
+        response_data = self.app.get('/max?X=4,5,56,78')
+        self.assertEqual(b'78.0\n', response_data.data)
+
+    def test_max(self):
+        response_data = self.app.get('/max?X=1,2,5,0,100'')
+        self.assertEqual(b'100.0\n', response_data.data)
+
+if __name__ == '__main__':
+    unittest.main()
+    
