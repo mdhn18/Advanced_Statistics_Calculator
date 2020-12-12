0ae30063f929d5944469543930bbfeeca77229d2
samir purno <purno.samir@gmail.com>
2020-12-12 20:27:07 +0100

    code is update

--- tests.py
+++ tests.py
@@ -25,7 +25,42 @@ class TestOnlineCalculator(unittest.TestCase):
     def test_max(self):
         response_data = self.app.get('/max?X=4,5,56,78')
         self.assertEqual(b'78.0\n', response_data.data)
-        
+
+    def test_max(self):
+        response_data = self.app.get('/max?X=1,2,5,0,100'')
+        self.assertEqual(b'100.0\n', response_data.data)
+
+    def test_mean(self):
+        response_data = self.app.get('/mean?X=1,2,5,0,100')
+        self.assertEqual(b'21.6\n', response_data.data)
+
+    def test_mean(self):
+        response_data = self.app.get('/mean?X=1000,2.5,100')
+        self.assertEqual(b'367.5\n', response_data.data)
+
+    def test_avg(self):
+        response_data = self.app.get('/avg?X=1,2,5,0,100')
+        self.assertEqual(b'21.6\n', response_data.data)
+
+    def test_average(self):
+        response_data = self.app.get('/average?X=1,2,5,0,100')
+        self.assertEqual(b'21.6\n', response_data.data)
+
+    def test_min(self):
+        response_data = self.app.get('/min?X=1,4,9,0,50')
+        self.assertEqual(b'1.0\n', response_data.data)
+
+    def test_min(self):
+        response_data = self.app.get('/min?X=1,2,5,0,100,-100,-5,-2')
+        self.assertEqual(b'-100.0\n', response_data.data)
+
+    def test_median(self):
+        response_data = self.app.get('/median?X=1,2,5,0,100,-100,-5,-2')
+        self.assertEqual(b'0.5\n', response_data.data)
+
+    def test_median(self):
+        response_data = self.app.get('/median?X=4,5,56,78')
+        self.assertEqual(b'0.5\n', response_data.data)
 if __name__ == '__main__':
     unittest.main()
     
